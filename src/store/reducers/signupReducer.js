5533d0ce95f29f339ec175f2e8da24c4863164c5
ndurbin <nick.durbin@gmail.com>
2019-11-14 13:29:49 -0500

    Setup an initial reducer for signing up.

---
+++ src/store/reducers/signupReducer.js
@@ -0,0 +1,35 @@
+import { GET_FETCH_REQUEST, GET_FETCH_SUCCESS, GET_FETCH_ERROR } from '../actions/signupActions';
+
+const initialState = {
+  isLoading: false,
+  error: '',
+  books: []
+}
+
+export function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_SIGNUP_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        users: null,
+        error: null
+      }
+    case FETCH_SIGNUP_SUCCESS:
+      return {
+        ...state,
+          isLoading: false,
+          users: action.payload,
+          error: null
+      }
+    case FETCH_SIGNUP_ERROR:
+      return {
+        ...state,
+        isLoading: true,
+        users: null,
+        error: action.payload
+      }
+    default:
+      return state
+    }
+}
